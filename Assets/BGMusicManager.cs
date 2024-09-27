94da3d0c879fa3801e7cf8b207a457d0f07cb675
Giocumber <giosalceda@gmail.com>
2024-09-27 13:34:14 +0800

    Build levels

--- Assets/BGMusicManager.cs
+++ Assets/BGMusicManager.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class BGMusicManager : MonoBehaviour
 {
+    private static BGMusicManager instance;
+
     [Header("------------- Audio Source -------------")]
     public AudioSource BGMusicSource;
 
     [Header("------------- Audio Clip -------------")]
     public AudioClip BGMusic;
+
+    void Awake()
+    {
+        // Check if an instance of AudioManager already exists
+        if (instance == null)
+        {
+            instance = this; // If not, set this instance
+            DontDestroyOnLoad(gameObject); // Make this instance persist across scenes
+        }
+        else
+        {
+            Destroy(gameObject); // If an instance already exists, destroy the duplicate
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -24,4 +40,5 @@ public class BGMusicManager : MonoBehaviour
         BGMusicSource.Play();
     }
 
+    
 }
