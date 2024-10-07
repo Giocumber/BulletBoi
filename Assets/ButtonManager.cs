36b67745eea37115664eac2f86bc86414e00fc27
Giocumber <giosalceda@gmail.com>
2024-10-07 19:46:38 +0800

    Fix Bug and Added main menu button

--- Assets/ButtonManager.cs
+++ Assets/ButtonManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class ButtonManager : MonoBehaviour
@@ -93,5 +94,10 @@ public class ButtonManager : MonoBehaviour
     {
         elapsedTime = 0f;  // Reset the time
         isSliding = true;   // Start sliding
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
